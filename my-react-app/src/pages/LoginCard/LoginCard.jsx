b9fe9ba579ae3003311e176d95083da717df24eb
sabele90 <sabele@hotmail.es>
2023-11-12 20:11:15 +0000

    sin terminar

--- my-react-app/src/pages/LoginCard/LoginCard.jsx
+++ my-react-app/src/pages/LoginCard/LoginCard.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import "./LoginCard.css";
 import {
@@ -11,79 +10,97 @@ import {
   IconButton,
   InputAdornment,
   TextField,
-
 } from "@mui/material";
 import { Email, Lock, Visibility, VisibilityOff } from "@mui/icons-material";
 
 export default function LoginCard() {
-    const [visible,setVisible]= useState (false)
+  const [visible, setVisible] = useState(false);
   return (
     <>
-   
-    <div className="bodyLogin">
-    <img className="logo" src="./public/images/logo.png" alt="logo" width="100"></img>
-      <Card 
-        sx ={{ padding: "50px",margin: "100px", maxWidth: "700px", backgroundColor: "white" ,borderRadius: '10px', display: "flex", flexDirection:"column", justifyContent:"center", textAlign:"center" ,marginTop: "100px"}}
-        raised={true}
-      >
-        <CardHeader title="Login"
-        sx={{ marginTop: "10px"}}
-        ></CardHeader>
+      <div className="bodyLogin">
+        <img
+          className="logo"
+          src="./public/images/logo.png"
+          alt="logo"
+          width="100"
+        ></img>
+        <Card
+          sx={{
+            width: "25%",
+            padding: "5%", // Utilizando un porcentaje para el padding
+            margin: "5%", // Utilizando un porcentaje para el margin
+            maxWidth: "70%", // Utilizando un porcentaje para el maxWidth
+            backgroundColor: "rgba(255, 255, 255, 0.5)",
+            borderRadius: "10px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+          raised={true}
+        >
+          <CardHeader
+            title="Welcome back!"
+            sx={{ marginTop: "10px" }}
+          ></CardHeader>
 
-        <CardContent>
-          <TextField
-            margin="dense"
-            fullWidth={true}
-            variant="outlined"
-            type="email"
-            label="Email"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment>
-                  <Icon>
-                    {" "}
-                    <Email />
-                  </Icon>{" "}
-                </InputAdornment>
-              ),
-            }}
-          ></TextField>
-          <TextField
-            margin="dense"
-            fullWidth={true}
-            variant="outlined"
-            type={visible ? "text" : "password"}
-            label="Password"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment>
-                  <Icon>
-                    {" "}
-                    <Lock />
-                  </Icon>
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment>
-                  <IconButton onClick={()=> setVisible(!visible)}>
-                    {" "}
-                   { visible ? <Visibility /> : <VisibilityOff/>}
-                  </IconButton>{" "}
-                </InputAdornment>
-              ),
-            }}
-          ></TextField>
-        </CardContent>
+          <CardContent>
+            <TextField
+              margin="dense"
+              fullWidth={true}
+              variant="outlined"
+              type="email"
+              label="Email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <Icon>
+                      {" "}
+                      <Email />
+                    </Icon>{" "}
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <TextField
+              margin="dense"
+              fullWidth={true}
+              variant="outlined"
+              type={visible ? "text" : "password"}
+              label="Password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <Icon>
+                      {" "}
+                      <Lock />
+                    </Icon>
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment>
+                    <IconButton onClick={() => setVisible(!visible)}>
+                      {" "}
+                      {visible ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>{" "}
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+          </CardContent>
 
-        <CardActions  sx={{display:"flex", justifyContent:"center"}}>
-          <Button size="large" color="primary" variant="contained">
-            Login
-          </Button>
-        
+          <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              className="button"
+              size="large"
+              color="primary"
+              variant="contained"
+            >
+              Login
+            </Button>
           </CardActions>
-        
-      </Card>
+        </Card>
       </div>
-      </>
+    </>
   );
 }
--- my-react-app/src/pages/RegisterCard/SignUpCard.jsx
+++ my-react-app/src/pages/RegisterCard/SignUpCard.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import './SignUpCard.css'
-
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SignUpCard.css";
+import { Card, CardContent, CardHeader } from "@mui/material";
+import avatarImage from "../../../public/images/6681204.png";
 const SingUpCard = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    birthdate: "",
   });
 
   const handleChange = (e) => {
@@ -22,57 +24,118 @@ const SingUpCard = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Aquí puedes realizar acciones como enviar los datos al servidor
-    console.log('Form submitted:', formData);
+    console.log("Form submitted:", formData);
   };
 
   return (
     <>
-    <div className='bodySignUp'>
-    <div className='containerSignUp'>
-      <h2>Singup</h2>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Usuario"
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Correo Electrónico"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Contraseña"
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Confirmar Contraseña"
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <Button type="submit" variant="contained" color="primary">
-          Registrarse
-        </Button>
-      </form>
-    </div>
-    </div>
+      <div className="bodySignUp">
+        <div className="containerSignUp">
+          <Card
+            sx={{
+              width: "40%",
+              padding: "5%", // Utilizando un porcentaje para el padding
+              margin: "5%", // Utilizando un porcentaje para el margin
+              maxWidth: "70%", // Utilizando un porcentaje para el maxWidth
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              borderRadius: "10px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+            raised={true}
+          >
+            <CardHeader
+              title="Sign up"
+              sx={{ marginTop: "10px", position: "relative" }}
+            >
+              <img
+                src={avatarImage} // Ajusta la ruta de tu imagen
+                alt="Avatar"
+                style={{
+                  borderRadius: "50%",
+                  width: "80px", // Ajusta el tamaño de tu imagen
+                  height: "80px", // Ajusta el tamaño de tu imagen
+                  position: "absolute",
+                  top: "-40px",
+                }}
+              />
+            </CardHeader>
+
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="Name"
+                  type="text"
+                  name="name"
+                  value={formData.username}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Surname"
+                  type="text"
+                  name="surname"
+                  value={formData.username}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Correo Electrónico"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Contraseña"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Confirmar Contraseña"
+                  type="password"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="About you"
+                  type="about"
+                  name="about you"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="dense"
+                />
+                <TextField
+                  label="Interest"
+                  type="interest"
+                  name="interest"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  fullWidth
+                  margin="dense"
+                />
+                <Button type="submit" variant="contained" color="primary">
+                  Registrarse
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
